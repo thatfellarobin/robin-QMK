67a235d2962d8e757a697f2eaade89c6b4a998a8
Robin Liu <Robin.Liu.831@gmail.com>
2019-04-13 22:01:45 -0400

    Fixed R4 alignment issue

--- README.md
+++ README.md
@@ -3,6 +3,4 @@ Howdy, this is firmware for my keyboard that uses a DZ60 PCB.
 
 ## Known issues:
 - Numpad layer doesn't toggle off
-  - Might have fixed this by using TG() instead of TO()
-- Misaligned R4, probably to do with the splittable left shift
-  - Further testing required.
+  - Did not fix this by using TG() instead of TO()
--- keymap.c
+++ keymap.c
@@ -17,11 +17,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * | Ctrl |  Alt  |  Cmd  |               Space               | Cmd | Shf |  L  |  D  |  R   |
 	 * `-----------------------------------------------------------------------------------------'
 	 */
-	LAYOUT(
+	LAYOUT_directional(
 		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_GRV, KC_DEL,
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPACE,
 		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT,         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_BSLASH, KC_UP, MO(1),
+		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_BSLASH, KC_UP, MO(1),
 		KC_LCTL, KC_LALT, KC_LGUI, KC_NO, KC_SPC, KC_NO, KC_RGUI, KC_RSFT, KC_LEFT, KC_DOWN, KC_RIGHT),
 
 	/* LAYER 1: Fn Layer
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |      |      |      |                                     |     | Lay2 |     |     |     |
 	 * `-----------------------------------------------------------------------------------------'
 	 */
-	LAYOUT(
+	LAYOUT_directional(
 		______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, ______, ______,
 		______, RGB_TOG, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, RESET,
 		______, ______, ______, ______, ______, ______, ______, ______, KC_SYSTEM_SLEEP, ______, ______, ______, ______,
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 * |      |      |      |                                     |  0  |     |     |     |      |
 	 * `-----------------------------------------------------------------------------------------'
 	 */
-	LAYOUT(
+	LAYOUT_directional(
  		______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
  		______, ______, ______, ______, ______, ______, ______, ______, KC_7, KC_8, KC_9, ______, ______, KC_NO,
  		______, ______, ______, ______, ______, ______, ______, ______, KC_4, KC_5, KC_6, KC_ENT, ______,
